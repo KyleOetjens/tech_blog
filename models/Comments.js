003c9a8015bb094212894e5885aa3a8e792f4e94
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-03-17 17:54:48 -0500

    initcommit

---
+++ models/Comments.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comment_body: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comments',
+  }
+);
+
+module.exports = Comments;
