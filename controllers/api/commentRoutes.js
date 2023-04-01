2d1cfa79d3dd86b99ec665b763ff4232b92bc5e9
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-03-31 20:52:47 -0500

    commit

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -18,7 +18,7 @@ router.post('/', withAuth, async (req, res) => {
     }
   });
   
-  router.delete('/:id', withAuth, async (req, res) => {
+  router.delete('/:id', async (req, res) => {
     try {
       const postData = await Comment.destroy({
         where: {
@@ -32,7 +32,7 @@ router.post('/', withAuth, async (req, res) => {
         return;
       }
   
-      res.status(200).json(postData);
+      res.status(200).json(postData`comment deleted`);
     } catch (err) {
       res.status(500).json(err);
     }
